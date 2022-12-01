027e3884eed6f34177f4d8e314dc9b782aaab8c4
aripo <aripovsergey3@gmail.com>
2022-12-01 17:41:02 +0500

    Поиск видео батчами

--- Treplo/Youtube/YoutubeEngine.cs
+++ Treplo/Youtube/YoutubeEngine.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using Treplo.Models;
 using YoutubeExplorer.Common;
+using YoutubeExplorer.Search;
 using YoutubeExplorer.Videos.Streams;
 
 namespace Treplo.Youtube;
@@ -13,31 +14,45 @@ public class YoutubeEngine : ISearchEngine
     {
         this.clientFactory = clientFactory;
     }
-    
-    async IAsyncEnumerable<Track> ISearchEngine.FindInternalAsync(string query, [EnumeratorCancellation]CancellationToken cancellationToken)
+
+    async IAsyncEnumerable<Track> ISearchEngine.FindInternalAsync(string query,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
     {
         var client = CreateClient();
-        var result = client.Search.GetVideosAsync(query, cancellationToken);
-        await foreach (var video in result.WithCancellation(cancellationToken))
+        var result = client.Search.GetResultBatchesAsync(query, SearchFilter.Video, cancellationToken);
+        await foreach (var batch in result.WithCancellation(cancellationToken))
         {
-            var duration = video.Duration;
-            if (duration is null)
-                continue;
-            var manifest = await client.Videos.Streams.GetManifestAsync(video.Id, cancellationToken);
-            var audioStreamInfo = manifest
-                .GetAudioOnlyStreams()
-                .TryGetWithHighestBitrate() as IAudioStreamInfo;
-            if (audioStreamInfo is null)
-                continue;
-            yield return new Track
+            var videos = batch.Items
+                .OfType<VideoSearchResult>()
+                .Where(x => x.Duration is not null)
+                .ToArray();
+            var manifestTasks = videos
+                .Select(x => client.Videos.Streams.GetManifestAsync(x.Id, cancellationToken).AsTask());
+
+            var manifests = await Task.WhenAll(manifestTasks);
+
+            foreach (var track in videos.Zip(manifests, CollectTrack).Where(track => track is not null))
             {
-                Author = video.Author.ChannelTitle,
-                Title = video.Title,
-                Thumbnail = video.Thumbnails.GetWithHighestResolution().Into(),
-                Source = audioStreamInfo.Into(),
-                Duration = duration.GetValueOrDefault(),
-            };
+                yield return track.GetValueOrDefault();
+            }
         }
+    }
+
+    private static Track? CollectTrack(VideoSearchResult video, StreamManifest manifest)
+    {
+        var audioStreamInfo = manifest
+            .GetAudioOnlyStreams()
+            .TryGetWithHighestBitrate() as IAudioStreamInfo;
+        if (audioStreamInfo is null)
+            return null;
+        return new Track
+        {
+            Author = video.Author.ChannelTitle,
+            Title = video.Title,
+            Thumbnail = video.Thumbnails.GetWithHighestResolution().Into(),
+            Source = audioStreamInfo.Into(),
+            Duration = video.Duration.GetValueOrDefault(),
+        };
     }
 
     public string Name => "Youtube";
